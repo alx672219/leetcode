b007aa5e82a9c5d2407aa1386f9578fb4cbe33ce
Alex <113051612+alx672219@users.noreply.github.com>
2023-08-07 23:55:09 -0400

    Time: 2392 ms (66.97%), Space: 16.7 MB (35.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,6 +1,7 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
+        dp = [1] * len(nums) #dp[i] represents the length of the longest increasing subsequence that ends with the ith element 
+
         for i in range(1, len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
