de38a8ba35b542b0f24e1610dc4e0978e3d6c627
Alex <113051612+alx672219@users.noreply.github.com>
2024-08-03 14:53:46 -0400

    Update 0002-add-two-numbers.py

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = dummy = ListNode()
+        tail = dummy = ListNode()
         carry = 0   # 8 + 6 = 14, carry is 1
         while l1 or l2:
             val1, val2 = 0, 0
@@ -18,10 +18,10 @@ class Solution:
             val = carry + val1 + val2
             carry = val // 10   # 8 + 6 = 14, carry = 1
             val = val % 10      # 8 + 6 = 14, val = 4
-            res.next = ListNode(val)
-            res = res.next
+            tail.next = ListNode(val)
+            tail = tail.next
             
         if carry:
-            res.next = ListNode(carry) # set list node to next val and set it to 1
-            
-        return dummy.next # cuz dummys default is 0 = listnode(0)
+            tail.next = ListNode(carry) # set list node to next val and set it to 1
+        head = dummy.next
+        return head 
